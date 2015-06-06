fa1b7255ae38129326cda5d16277eb6d88bab133
Brendon Morgan <sire_2_bah@hotmail.com>
2015-06-06 18:53:46 +1200

    Torch now has point light
    Changed the torch to use a point light and has the basic sin wave
    oscillation

--- Assets/Engine/Tile Engine/Torch.cs
+++ Assets/Engine/Tile Engine/Torch.cs
@@ -8,19 +8,19 @@ public class Torch : Dynamic_Object {
 	
 	protected override void Start() {
 		base.Start();
-		torch_light = Instantiate(light_prefab, transform.position, Quaternion.Euler(Vector3.zero)) as GameObject;
+		/*torch_light = Instantiate(light_prefab, transform.position, Quaternion.Euler(Vector3.zero)) as GameObject;
 		torch_light.transform.parent = gameObject.transform;
-		light_renderer = torch_light.GetComponent<SpriteRenderer>();
+		light_renderer = torch_light.GetComponent<SpriteRenderer>();*/
 		
 	}
 	
 	protected override void Update() {
 		base.Update();
 		//Check if we are "lit" and enable light_prefab
-		if(anim_state == 1 && light_renderer != null) {
+		/*if(anim_state == 1 && light_renderer != null) {
 			light_renderer.enabled = true;
 		} else {
 			light_renderer.enabled = false;
-		}
+		}*/
 	} 	
 }
---
+++ Assets/Light/Flickering_Point_Light.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flickering_Point_Light : MonoBehaviour {
+	private GameObject point_light;
+	private Light light_source;
+	public string wave_function = "sin";
+	public float start = 0.0f;
+	public float amplitude = 1.0f;
+	public float phase = 0.0f;
+	public float frequency = 0.5f;
+	
+	private Color original_colour;
+	private float original_intensity;
+
+	// Use this for initialization
+	void Start () {
+		light_source = GetComponent<Light>();;
+		original_colour = light_source.color;	
+		original_intensity = light_source.intensity;
+	}	
+	
+	void Update() {
+		//light_source.color = original_colour * (Eval_Wave());
+		light_source.intensity = original_intensity * (Eval_Wave());
+	}
+	
+	float Eval_Wave() {
+		float x = (Time.time + phase) * frequency;
+		float y;
+		
+		//Normalize x
+		x = x - Mathf.Floor(x);
+		
+		if(wave_function == "sin") {
+			y = Mathf.Sin(x * 2 * Mathf.PI);
+		} else {
+			y = 1.0f;
+		}
+		
+		return (y * amplitude) + start;
+	}
+}
